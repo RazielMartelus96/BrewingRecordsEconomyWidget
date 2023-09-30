3bc0dfcc95b4f5364aca4548ff3826994286e2e3
RazielMartelus96 <117315226+RazielMartelus96@users.noreply.github.com>
2023-09-30 22:21:52 +0100

    Completed SaveDialogBox.java documentation and populated the package-info.java of the controls package.

--- src/main/java/io/curiositycore/brewingrecordseconomywidget/gui/persistance/controls/SaveDialogBox.java
+++ src/main/java/io/curiositycore/brewingrecordseconomywidget/gui/persistance/controls/SaveDialogBox.java
@@ -2,7 +2,13 @@ package io.curiositycore.brewingrecordseconomywidget.gui.persistance.controls;
 
 import javafx.scene.control.TextInputDialog;
 
+/**
+ * Dialog box for choosing the file name when saving a Brew Configuration Preset.
+ */
 public class SaveDialogBox extends TextInputDialog {
+    /**
+     * Constructor for the dialog box which initialises the box's title, header and content text.
+     */
     public SaveDialogBox(){
 
         this.setTitle("Choose a file name");
--- src/main/java/io/curiositycore/brewingrecordseconomywidget/gui/persistance/controls/package-info.java
+++ src/main/java/io/curiositycore/brewingrecordseconomywidget/gui/persistance/controls/package-info.java
@@ -1,1 +1,4 @@
+/**
+ * Classes relevant to the controls of the JavaFX GUI.
+ */
 package io.curiositycore.brewingrecordseconomywidget.gui.persistance.controls;
