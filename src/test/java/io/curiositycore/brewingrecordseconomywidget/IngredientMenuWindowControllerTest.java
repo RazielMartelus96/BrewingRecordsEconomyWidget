ae68a10bc497d07b4e93de08e7bea9d58480a253
RazielMartelus96 <117315226+RazielMartelus96@users.noreply.github.com>
2023-09-30 17:36:43 +0100

    Addition of initial test classes for the two main controllers of the application.
    In addition, a test config.yml has been created, to be used during testing in future unit-test classes.

---
+++ src/test/java/io/curiositycore/brewingrecordseconomywidget/IngredientMenuWindowControllerTest.java
@@ -0,0 +1,16 @@
+package io.curiositycore.brewingrecordseconomywidget;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IngredientMenuWindowControllerTest {
+
+    @Test
+    void initialize() {
+    }
+
+    @Test
+    void onTableRowClick() {
+    }
+}
---
+++ src/test/java/io/curiositycore/brewingrecordseconomywidget/MainWindowControllerTest.java
@@ -0,0 +1,24 @@
+package io.curiositycore.brewingrecordseconomywidget;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainWindowControllerTest {
+
+    @Test
+    void initialize() {
+    }
+
+    @Test
+    void onRowClick() {
+    }
+
+    @Test
+    void onIngredientsListOpen() {
+    }
+
+    @Test
+    void saveBrewsToPersistentData() {
+    }
+}
