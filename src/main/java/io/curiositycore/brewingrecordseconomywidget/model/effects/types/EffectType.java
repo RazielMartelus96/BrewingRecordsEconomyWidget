9277e9dcb0f6f67b59199390fe2cb569e1fe2a8c
RazielMartelus96 <117315226+RazielMartelus96@users.noreply.github.com>
2023-10-01 11:09:27 +0100

    Completed documentation for:
    - EffectType.java
    - PositiveEffect.java
    - NegativeEffect.java

--- src/main/java/io/curiositycore/brewingrecordseconomywidget/model/effects/types/EffectType.java
+++ src/main/java/io/curiositycore/brewingrecordseconomywidget/model/effects/types/EffectType.java
@@ -1,5 +1,21 @@
 package io.curiositycore.brewingrecordseconomywidget.model.effects.types;
 
+/**
+ * Enumeration representing how an {@linkplain
+ * io.curiositycore.brewingrecordseconomywidget.model.effects.Effect Effect} is identified based on its influence on
+ * particular activities.
+ */
 public enum EffectType {
-    COMBAT,UTILITY,ROLEPLAY;
+    /**
+     * An Effect that influences the mechanics of in-game combat.
+     */
+    COMBAT,
+    /**
+     * An Effect that influences the general mechanics of in-game activities (e.g: Mining, Farming, Enchanting etc).
+     */
+    UTILITY,
+    /**
+     * An Effect that has generally superficial effects in-game, and hence are mainly for Roleplaying purposes.
+     */
+    ROLEPLAY
 }
--- src/main/java/io/curiositycore/brewingrecordseconomywidget/model/effects/types/PositiveEffect.java
+++ src/main/java/io/curiositycore/brewingrecordseconomywidget/model/effects/types/PositiveEffect.java
@@ -1,6 +1,8 @@
 package io.curiositycore.brewingrecordseconomywidget.model.effects.types;
 
 import io.curiositycore.brewingrecordseconomywidget.model.effects.Effect;
-
+/**
+ * An Effect that influences in-game mechanics in a Positive way.
+ */
 public interface PositiveEffect extends Effect {
 }
